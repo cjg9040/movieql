6ed28249902897d1a69bdc3a44afb4d8dd770550
cjg9040 <chl8273@kookmin.ac.kr>
2019-01-08 23:36:36 +0900

    this is graphql tut

---
+++ graphql/db.js
@@ -0,0 +1,47 @@
+let movies = [{
+        id: 0,
+        name: "star wars",
+        score: 1
+    },
+    {
+        id: 1,
+        name: "avengers",
+        score: 8
+    },
+    {
+        id: 2,
+        name: "godfather",
+        score: 99
+    },
+    {
+        id: 3,
+        name: "logan",
+        score: 2
+    }
+];
+
+export const getMovies = () => movies;
+export const getById = id => {
+    const filteredMovies = movies.filter(movie => movie.id === (id));
+    return filteredMovies[0];
+}
+
+export const deleteMovie = id => {
+    const cleanMovies = movies.filter(movie => movie.id !== (id));
+    if (movies.length > cleanMovies.length) {
+        movies = cleanMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length+1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+};
